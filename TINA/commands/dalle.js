2b8558d42fe02689dfd0c7f260ed54a0bad519dc
SANJIDA <khansanjida895@gmail.com>
2025-05-08 14:17:59 +0600

    Update dalle.js

--- TINA/commands/dalle.js
+++ TINA/commands/dalle.js
@@ -15,7 +15,7 @@ module.exports = {
     const prompt = (event.messageReply?.body.split("dalle")[1] || args.join(" ")).trim();
     if (!prompt) return api.sendMessage("❌| Wrong Format. ✅ | Use: 17/18 years old boy/girl watching football match on TV with 'Khan Rahul RK' and '69' written on the back of their dress, 4k", event.threadID, event.messageID);
     try {
-  const cookies = ["1XI6YgQ8dxHsRw9sIt1LJ6DqQ50cCv6GaJB8wzhPnRcnEO7tbTdSEpIs1QBClP7rnaQ85DxbNKmEyAP59AUS6gUTkeTOZBnyz0fAbhxe2AqltHb9FnxrSJJzEJMsJeqQ-bvuZXUUxk1hQwJ0mOkWOw2rRj1x-uDI0spLy7N0cfS1u5eUsijKzkCYEk5fh3PkSzPWTR9pVWI1BRpYILip-mQ"];
+  const cookies = ["1htAT0CzbppiwCfNYcBNoVIEpMC5isPzfpPaH5Rvmuza53sr563AuG9Ju_NktK3rb0a3CCbAANLvZ6UD_rdM6Jbu_T1N5DSfEM5IG6K6Sc6B4xFsdznbMicD_NPZipZ6WKQmXlTlXaqMaOBSpsSFP8NStwrR3kMQiseW5yTY8QlblIUGlP1DD9FMp31w0CDlBKPCuFE62jDxHqRfE-rH6VQ"];
 const randomCookie = cookies[Math.floor(Math.random() * cookies.length)];
       const wait = api.sendMessage("𝐏𝐥𝐞𝐚𝐬𝐞 𝐖𝐚𝐢𝐭𝐢𝐧𝐠 𝐏𝐫𝐨𝐜𝐞𝐬𝐬𝐢𝐧𝐠 𝐘𝐨𝐮𝐫 𝐈𝐦𝐚𝐠𝐞 \n\n𝐊𝐡𝐚𝐧 𝐑𝐚𝐡𝐮𝐥 𝐑𝐊", event.threadID);
       const response = await axios.get(`https://www.noobs-api.rf.gd/dipto/dalle?prompt=${prompt}&key=dipto008&cookie=${cookies}`);
