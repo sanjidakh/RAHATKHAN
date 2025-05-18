7da9c98ed5fcbef8d5a9d698a83cbbcffc0c4d76
SANJIDA <khansanjida895@gmail.com>
2025-05-18 23:41:14 +0600

    Update pair9.js

--- TINA/commands/pair9.js
+++ TINA/commands/pair9.js
@@ -22,7 +22,7 @@ module.exports.onLoad = async() => {
   const dirMaterial = __dirname + `/cache/canvas/`;
   const path = resolve(__dirname, 'cache/canvas', 'mira.png');
   if (!existsSync(dirMaterial + "canvas")) mkdirSync(dirMaterial, { recursive: true });
-  if (!existsSync(path)) await downloadFile("https://i.imgur.com/RDExQS7.jpg", path); 
+  if (!existsSync(path)) await downloadFile("https://i.postimg.cc/htdJQCqL/RDExQS7.jpg", path); 
 }
 
 async function makeImage({ one, two }) {
