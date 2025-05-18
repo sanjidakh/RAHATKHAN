31207f4ed759ffb3a64b830ea8406ebc41c4fe94
SANJIDA <khansanjida895@gmail.com>
2025-05-18 23:17:57 +0600

    Update gf.js

--- TINA/commands/gf.js
+++ TINA/commands/gf.js
@@ -22,7 +22,7 @@ module.exports.onLoad = async() => {
     const dirMaterial = __dirname + `/cache/canvas/`;
     const path = resolve(__dirname, 'cache/canvas', 'arr2.png');
     if (!existsSync(dirMaterial + "canvas")) mkdirSync(dirMaterial, { recursive: true });
-    if (!existsSync(path)) await downloadFile("https://postimg.cc/1ng3h434", path); 
+    if (!existsSync(path)) await downloadFile("https://i.postimg.cc/vTq6sMFZ/iaOiAXe.jpg", path); 
 }
 
 async function makeImage({ one, two }) {
