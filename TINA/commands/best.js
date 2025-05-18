13bb5c31508c8ffcabb2faeb57886eb32a87555d
SANJIDA <khansanjida895@gmail.com>
2025-05-18 11:32:46 +0600

    Update best.js

--- TINA/commands/best.js
+++ TINA/commands/best.js
@@ -22,7 +22,7 @@ module.exports.onLoad = async() => {
   const dirMaterial = __dirname + `/cache/canvas/`;
   const path = resolve(__dirname, 'cache/canvas', 'ar.jpeg');
   if (!existsSync(dirMaterial + "canvas")) mkdirSync(dirMaterial, { recursive: true });
-  if (!existsSync(path)) await downloadFile("https://i.imgur.com/YEPhzxM.jpeg", path);
+  if (!existsSync(path)) await downloadFile("https://postimg.cc/tnk3jnhR", path);
 }
 
 async function makeImage({ one, two }) {
