9fbeb20064212948e68ed8d1184590f968d9d6ed
SANJIDA <khansanjida895@gmail.com>
2025-05-18 23:25:08 +0600

    Update secret5.js

--- TINA/commands/secret5.js
+++ TINA/commands/secret5.js
@@ -27,7 +27,7 @@ module.exports.run = async function({ api, event,Threads, Users }) {
         let Avatar = (await axios.get( `https://graph.facebook.com/${event.senderID}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: "arraybuffer" } )).data; 
             fs.writeFileSync( __dirname + "/cache/avt.png", Buffer.from(Avatar, "utf-8") );
 
-        let gifLove = (await axios.get( `https://postimg.cc/yk5qtnSh`, { responseType: "arraybuffer" } )).data; 
+        let gifLove = (await axios.get( `https://i.postimg.cc/8zk1F0pY/bhIsKO3.jpg`, { responseType: "arraybuffer" } )).data; 
             fs.writeFileSync( __dirname + "/cache/giflove.png", Buffer.from(gifLove, "utf-8") );
 
         let Avatar2 = (await axios.get( `https://graph.facebook.com/${id}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: "arraybuffer" } )).data;
