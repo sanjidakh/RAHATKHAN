a901622821cb0cbe5742ce6f2c3cd45a670b273b
SANJIDA <khansanjida895@gmail.com>
2025-05-18 23:37:32 +0600

    Update pair6.js

--- TINA/commands/pair6.js
+++ TINA/commands/pair6.js
@@ -22,7 +22,7 @@ module.exports.onLoad = async() => {
   const dirMaterial = __dirname + `/cache/canvas/`;
   const path = resolve(__dirname, 'cache/canvas', 'lovep.png');
   if (!existsSync(dirMaterial + "canvas")) mkdirSync(dirMaterial, { recursive: true });
-  if (!existsSync(path)) await downloadFile("https://i.imgur.com/VKvcNUA.jpg", path); 
+  if (!existsSync(path)) await downloadFile("https://i.postimg.cc/0yBfPX07/VKvcNUA.jpg", path); 
 }
 
 async function makeImage({ one, two }) {
