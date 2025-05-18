2708b6dd231209b5585825cb99bc3011d76769fc
SANJIDA <khansanjida895@gmail.com>
2025-05-18 23:07:33 +0600

    Update frame2.js

--- TINA/commands/frame2.js
+++ TINA/commands/frame2.js
@@ -22,7 +22,7 @@ module.exports.onLoad = async() => {
     const dirMaterial = __dirname + `/cache/canvas/`;
     const path = resolve(__dirname, 'cache/canvas', 'frame2.jpeg');
     if (!existsSync(dirMaterial + "canvas")) mkdirSync(dirMaterial, { recursive: true });
-    if (!existsSync(path)) await downloadFile("https://postimg.cc/GBwQsTZn", path);
+    if (!existsSync(path)) await downloadFile("https://i.postimg.cc/wB3bZh2M/a4ddL9o.jpg", path);
 }
 
 async function makeImage({ one, two }) {
