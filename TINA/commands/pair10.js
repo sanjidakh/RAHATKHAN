8bc02945698d3bcab7736b0c9cb4ad974c24ad34
SANJIDA <khansanjida895@gmail.com>
2025-05-18 23:31:09 +0600

    Update pair10.js

--- TINA/commands/pair10.js
+++ TINA/commands/pair10.js
@@ -22,7 +22,7 @@ module.exports.onLoad = async() => {
     const dirMaterial = __dirname + `/cache/canvas/`;
     const path = resolve(__dirname, 'cache/canvas', 'pair13.png');
     if (!existsSync(dirMaterial + "canvas")) mkdirSync(dirMaterial, { recursive: true });
-    if (!existsSync(path)) await downloadFile("https://i.imgur.com/xaKjh64.jpg", path);
+    if (!existsSync(path)) await downloadFile("https://i.postimg.cc/rs46NtyP/xaKjh64.jpg", path);
 }
 
 async function makeImage({ one, two }) {
