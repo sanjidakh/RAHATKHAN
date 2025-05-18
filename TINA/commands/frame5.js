5ed1064ecc18f5d08d34ca9441754f1d33c1c019
SANJIDA <khansanjida895@gmail.com>
2025-05-18 11:56:25 +0600

    Update frame5.js

--- TINA/commands/frame5.js
+++ TINA/commands/frame5.js
@@ -22,7 +22,7 @@ module.exports.onLoad = async() => {
     const dirMaterial = __dirname + `/cache/canvas/`;
     const path = resolve(__dirname, 'cache/canvas', 'frame5.jpeg');
     if (!existsSync(dirMaterial + "canvas")) mkdirSync(dirMaterial, { recursive: true });
-    if (!existsSync(path)) await downloadFile("https://i.imgur.com/pGMF7YD.jpg", path);
+    if (!existsSync(path)) await downloadFile("https://postimg.cc/p9RjHfwr", path);
 }
 
 async function makeImage({ one, two }) {
