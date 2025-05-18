40406809f5234fff86bb122e7dbe670337ffcbc4
SANJIDA <khansanjida895@gmail.com>
2025-05-18 11:44:31 +0600

    Update darling3.js

--- TINA/commands/darling3.js
+++ TINA/commands/darling3.js
@@ -59,7 +59,7 @@ module.exports.run = async function ({ args, Users, Threads, api, event, Currenc
   var tile = djtme[Math.floor(Math.random() * djtme.length)];
 
   var background = [
-  "https://imgur.com/a/h3aDDT8",
+  "https://postimg.cc/s1kRxwwn",
   ];
   var rd = background[Math.floor(Math.random() * background.length)];
 
