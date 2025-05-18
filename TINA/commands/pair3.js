254caa935e017488d3c7c8ba283282ff263a5a30
SANJIDA <khansanjida895@gmail.com>
2025-05-18 23:33:53 +0600

    Update pair3.js

--- TINA/commands/pair3.js
+++ TINA/commands/pair3.js
@@ -22,7 +22,7 @@ module.exports.onLoad = async() => {
   const dirMaterial = __dirname + `/cache/canvas/`;
   const path = resolve(__dirname, 'cache/canvas', 'pair3.png');
   if (!existsSync(dirMaterial + "canvas")) mkdirSync(dirMaterial, { recursive: true });
-  if (!existsSync(path)) await downloadFile("https://i.imgur.com/tWuWCF6.jpg", path);
+  if (!existsSync(path)) await downloadFile("https://i.postimg.cc/3wXkzBfN/tWuWCF6.jpg", path);
 }
 
 async function makeImage({ one, two }) {
